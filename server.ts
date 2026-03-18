48524c502e2ecd15cdb14b760a97e7a01dce4203
rlaxogh76 <btm.email2769@gmail.com>
2026-03-18 15:19:44 +0900

    feat: 인터뷰 데이터 구조를 정의하는 인터페이스 추가

---
+++ server.ts
@@ -0,0 +1,11 @@
+export interface interview {
+  id: string;
+  author: string;
+  authorUrl: string;
+  tag: string;
+  question: string;
+  answer: string;
+  linkName: string;
+  linkUrl: string;
+  thumbnailUrl: string;
+}
