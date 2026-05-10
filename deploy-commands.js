d17afd14ee1742fddf3f6cc946b281dfb9c15cb2
rlaxogh76 <btm.email2769@gmail.com>
2026-05-10 16:19:19 +0900

    refactor: 명령어 파일 로딩 로직 개선

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,29 +3,29 @@ const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const commands = [];
-// Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+function loadCommandFiles(dirPath) {
+	const files = [];
+	for (const entry of fs.readdirSync(dirPath)) {
+		const fullPath = path.join(dirPath, entry);
+		if (fs.statSync(fullPath).isDirectory()) {
+			files.push(...loadCommandFiles(fullPath));
+		}
+		else if (entry.endsWith('.js')) {
+			files.push(fullPath);
+		}
+	}
+	return files;
+}
 
-for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs
-		.readdirSync(commandsPath)
-		.filter((file) => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		}
-		else {
-			console.log(
-				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-			);
-		}
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+for (const filePath of loadCommandFiles(foldersPath)) {
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		commands.push(command.data.toJSON());
+	}
+	else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
 
