8b6f05ab2c021fb17addade88ff36815683a2c81
rlaxogh76 <btm.email2769@gmail.com>
2026-05-10 23:02:14 +0900

    fix: 로거를 사용하여 사용자 명령 실행 오류 메시지 개선

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -9,7 +9,7 @@ module.exports = {
 			await interaction.reply(`This command runned by ${interaction.user.username}, who joined at ${interaction.user.createdAt}`);
 		}
 		catch (error) {
-			console.error('Error executing user command:', error);
+			logger.error('Error while executing command:', error);
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	},
